1e5d82d4c6e35f0fd16f0b0cfafa934e62bb7a3a
DESKTOP-8346GUH\Valley1 <interns@valleyit.us>
2018-04-10 13:26:24 -0500

    Added scheduled email functionality

--- FinalHotelProject/Global.asax.cs
+++ FinalHotelProject/Global.asax.cs
@@ -16,6 +16,7 @@ using System.Web.Mail;
 using System.Web.Caching;
 using System.IO;
 using System.Net.Mail;
+using HotelDBApp;
 
 namespace FinalHotelProject
 {
@@ -26,11 +27,28 @@ namespace FinalHotelProject
         private const string DummyCacheItemKey = "GagaGuguGigi";
         public Global()
         {
-            InitializeComponent();
+            //InitializeComponent();
+        }
+        private DataSet FetchUserEmails()
+        {
+            return Email.FetchEmails();
         }
         protected void Application_Start(object sender, EventArgs e)
         {
-            RegisterCacheEntry();
+            //RegisterCacheEntry();
+            DateTime startDate = DateTime.Now;
+            DateTime endDate = startDate.AddMinutes(1);
+            System.Timers.Timer myTimer = new System.Timers.Timer();
+            // Set the Interval to 5 seconds (5000 milliseconds).
+            
+            myTimer.Interval = (endDate - startDate).TotalMilliseconds;
+            myTimer.AutoReset = true;
+            myTimer.Elapsed += new System.Timers.ElapsedEventHandler(myTimer_Elapsed);
+            myTimer.Enabled = true;
+        }
+        public void myTimer_Elapsed(object source, System.Timers.ElapsedEventArgs e)
+        {
+           DoSomeEmailSendStuff();
         }
         private void RegisterCacheEntry()
         {
@@ -80,34 +98,42 @@ namespace FinalHotelProject
         }
         private void DoSomeEmailSendStuff()
         {
-            try
+            DataSet ds = FetchUserEmails();
+            foreach (DataRow item in ds.Tables[0].Rows)
             {
-                System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage();
-                mail.From = new System.Net.Mail.MailAddress("interns@valleyit.us");
+                try
+                {
+                    using (System.Net.Mail.MailMessage mail = new System.Net.Mail.MailMessage())
+                    {
+                        mail.From = new System.Net.Mail.MailAddress("interns@valleyit.us");
 
-                // The important part -- configuring the SMTP client
-                SmtpClient smtp = new SmtpClient();
-                smtp.Port = 587;   // [1] You can try with 465 also, I always used 587 and got success
-                smtp.EnableSsl = true;
-                smtp.DeliveryMethod = SmtpDeliveryMethod.Network; // [2] Added this
-                smtp.UseDefaultCredentials = false; // [3] Changed this
-                smtp.Credentials = new NetworkCredential(mail.From.ToString(), "Intern@123");  // [4] Added this. Note, first parameter is NOT string.
-                smtp.Host = "smtp.gmail.com";
+                        // The important part -- configuring the SMTP client
+                        SmtpClient smtp = new SmtpClient();
+                        smtp.Port = 587;   // [1] You can try with 465 also, I always used 587 and got success
+                        smtp.EnableSsl = true;
+                        smtp.DeliveryMethod = SmtpDeliveryMethod.Network; // [2] Added this
+                        smtp.UseDefaultCredentials = false; // [3] Changed this
+                        smtp.Credentials = new NetworkCredential(mail.From.ToString(), "Intern@123");  // [4] Added this. Note, first parameter is NOT string.
+                        smtp.Host = "smtp.gmail.com";
 
-                //recipient address
-                mail.To.Add(new MailAddress("interns@valleyit.us"));
+                        //recipient address
+                        mail.To.Add(new MailAddress(item["Email"].ToString()));
 
-                //Formatted mail body
-                mail.IsBodyHtml = true;
-                string st = "Test";
+                        //Formatted mail body
+                        mail.IsBodyHtml = true;
+                        String link = String.Format("FeedbackForm.aspx?hotelid={0}",item["HotelID"].ToString());
+                        string st = String.Format("<p>Hi {0}. Thanks for staying with {1}.</p><p>Your feedback is valuable to us. Please spare a moment to provide your feedback for your stay at{2}</p><p>Please provide your feedback <a href={3}>here</a></p>",item["LastName"].ToString(), item["Property Name"].ToString(), item["Property Name"].ToString(),link);
 
-                mail.Body = st;
-                smtp.Send(mail);
+                        mail.Body = st;
+                        smtp.Send(mail);
+                    }
+                }
+                catch (Exception x)
+                {
+                    Debug.WriteLine(x);
+                }
             }
-            catch (Exception x)
-            {
-                Debug.WriteLine(x);
-            }
+           
         }
         protected void Session_Start(object sender, EventArgs e)
         {
@@ -116,17 +142,17 @@ namespace FinalHotelProject
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            String str = @"C:\Users\Valley1\Documents\ValleyIT\FinalHotelProject\MyDummyPage.aspx";
+            //String str = @"C:\Users\Valley1\Documents\ValleyIT\FinalHotelProject\MyDummyPage.aspx";
             //if (HttpContext.Current.Request.Url.ToString() == DummyPageUrl)
             //{
             //    // Add the item in cache and when succesful, do the work.
             //    RegisterCacheEntry();
             //}
-            if (@"C:\Users\Valley1\Documents\ValleyIT\FinalHotelProject\MyDummyPage.aspx" == DummyPageUrl)
-            {
+           // if (@"C:\Users\Valley1\Documents\ValleyIT\FinalHotelProject\MyDummyPage.aspx" == DummyPageUrl)
+           // {
                 // Add the item in cache and when succesful, do the work.
-                RegisterCacheEntry();
-            }
+               // RegisterCacheEntry();
+           // }
         }
 
         protected void Application_AuthenticateRequest(object sender, EventArgs e)
--- test/Email.cs
+++ test/Email.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace HotelDBApp
 {
@@ -15,5 +17,12 @@ namespace HotelDBApp
         public String ProblemValue { get; set; }
         public String Comments { get; set; }
         public DateTime CheckoutDate { get; set; }
+        public static DataSet FetchEmails()
+        {
+            SqlCommand cmd = new SqlCommand("SpFetchUserstoSendEmail");
+            cmd.CommandType = CommandType.StoredProcedure;
+            return DBOperations.FetchValues(cmd);
+        }
     }
+    
 }
