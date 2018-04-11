6d02182cf33f5083a2abd8de0093e3f275ed132c
DESKTOP-8346GUH\Valley1 <interns@valleyit.us>
2018-04-11 09:41:18 -0500

    sp for fetching email ids

---
+++ Database1/dbo/Stored Procedures/SpFetchUserstoSendEmail.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[SpFetchUserstoSendEmail]
+as
+ select H.[Property Name],U.HotelID, U.RoomNo,U.LastName,U.Email from Users U, Hotel H where convert(date,CheckOutDate)=convert(date,GETDATE()) and H.Property=U.HotelID
+
+
